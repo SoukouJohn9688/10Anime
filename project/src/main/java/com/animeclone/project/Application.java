1a1a091035ccc89b30d1799062de108b776e33d4
JuanCJ9688 <ironjohn788@gmail.com>
2024-06-28 11:08:14 -0500

    Created project structure.

---
+++ project/src/main/java/com/animeclone/project/Application.java
@@ -0,0 +1,13 @@
+package com.animeclone.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
