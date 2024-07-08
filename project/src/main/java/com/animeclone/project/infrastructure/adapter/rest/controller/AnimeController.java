6d91aa376925fb4c9df3faf5083e154a0d2f27ab
JuanCJ9688 <ironjohn788@gmail.com>
2024-07-08 14:58:17 -0500

    Probando 4

--- project/src/main/java/com/animeclone/project/infrastructure/adapter/rest/controller/AnimeController.java
+++ project/src/main/java/com/animeclone/project/infrastructure/adapter/rest/controller/AnimeController.java
@@ -1,4 +1,17 @@
 package com.animeclone.project.infrastructure.adapter.rest.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/anime")
 public class AnimeController {
+    @GetMapping
+    private String cargarLista(){
+        return "Probandooooooooo";
+    }
+
+
 }
