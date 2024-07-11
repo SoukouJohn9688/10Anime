afe3471391f8aad347ea349cdc57b1401dd2e764
JuanCJ9688 <ironjohn788@gmail.com>
2024-07-11 10:47:09 -0500

    Progressed on the anime services layer.

--- project/src/main/java/com/animeclone/project/application/mapper/AnimeMapper.java
+++ project/src/main/java/com/animeclone/project/application/mapper/AnimeMapper.java
@@ -5,26 +5,4 @@ import com.animeclone.project.domain.model.dto.AnimeDTO;
 
 public class AnimeMapper {
 
-    public static AnimeDTO toDTO(Anime anime) {
-        return new AnimeDTO(
-                anime.getDateAired(),
-                anime.getDuration(),
-                anime.getViews(),
-                anime.getPremiere(),
-                anime.getQuality(),
-                anime.getDescription()
-
-        );
-    }
-
-    public static Anime toEntity(AnimeDTO animeDTO) {
-        return Anime.builder()
-                .dateAired(animeDTO.getDateAired())
-                .description(animeDTO.getDescription())
-                .duration(animeDTO.getDuration())
-                .views(animeDTO.getViews())
-                .quality(animeDTO.getQuality())
-                .premiere(animeDTO.getPremiere())
-                .build();
-    }
 }
