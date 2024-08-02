9b598e257cfebb28169c7229baa77987d5e3ff25
Jorge1640 <jorgechasu@gmail.com>
2024-08-02 09:54:46 -0500

    edit genre

--- project/src/main/java/com/animeclone/project/infrastructure/adapter/rest/controller/GenreController.java
+++ project/src/main/java/com/animeclone/project/infrastructure/adapter/rest/controller/GenreController.java
@@ -3,8 +3,12 @@ package com.animeclone.project.infrastructure.adapter.rest.controller;
 import com.animeclone.project.application.usecases.GenreService;
 import com.animeclone.project.domain.model.dto.anime.RequestAnimeDTO;
 import com.animeclone.project.domain.model.dto.anime.ResponseAnimeDTO;
+import com.animeclone.project.domain.model.dto.episode.RequestEpisodeDTO;
+import com.animeclone.project.domain.model.dto.episode.ResponseEpisodeDTO;
 import com.animeclone.project.domain.model.dto.genre.RequestGenreDTO;
 import com.animeclone.project.domain.model.dto.genre.ResponseGenreDTO;
+import com.animeclone.project.infrastructure.adapter.exception.episode.EpisodeNotFoundException;
+import com.animeclone.project.infrastructure.adapter.exception.genre.GenreNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,6 +40,10 @@ public class GenreController {
     public void delete(@PathVariable Long id){
         genreService.deleteById(id);
 
+    }
+    @PostMapping("{id}")
+    private ResponseGenreDTO edit(@PathVariable Long id, @RequestBody RequestGenreDTO request) throws GenreNotFoundException {
+        return genreService.edit(id,request);
     }
 
 }
